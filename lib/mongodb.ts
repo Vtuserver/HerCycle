6cb554bf6c7a724754eee8adefbbbc17d2f407fe
unknown <abdullahidiagbonabdullahi@gmail.com>
2026-09-15 13:12:21 -0700

    Add non-null assertion for uri in mongoose.connect

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -7,7 +7,7 @@ const cached = global.mongooseCache ?? { conn:null, promise:null };
 global.mongooseCache = cached;
 export async function connectDB(){
   if(cached.conn) return cached.conn;
-  if(!cached.promise) cached.promise = mongoose.connect(uri);
+  if(!cached.promise) cached.promise = mongoose.connect(uri!);
   cached.conn = await cached.promise;
   return cached.conn;
 }
